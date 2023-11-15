1f3e205fbef902e63dfb057b2a9b3a77107dff71
Timothy Stokes <timstokes47@gmail.com>
2023-11-15 10:58:23 +0000

    GLFW and GLEW integrated into project

--- Physical Simulation/src/Physical Simulation.cpp
+++ Physical Simulation/src/Physical Simulation.cpp
@@ -1,12 +1,23 @@
 ﻿// Physical Simulation.cpp : Defines the entry point for the application.
 //
-
-#include "Physical Simulation.h"
-
-using namespace std;
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(960, 540, "Physical Simulation", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+	glewInit();
+	
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+	glfwTerminate();
 	return 0;
 }
