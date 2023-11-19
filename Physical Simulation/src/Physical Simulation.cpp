95a1423b8bbdd1a52466fb3029de89e6b7374bb9
Timothy Stokes <timstokes47@gmail.com>
2023-11-19 13:36:51 +0000

    Simulation controls added to sandbox using ImGui

--- Physical Simulation/src/Physical Simulation.cpp
+++ Physical Simulation/src/Physical Simulation.cpp
@@ -4,6 +4,11 @@
 #include "graphics/camera.h"
 #include "physics/particle.h"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_stdlib.h"
+
 #include <iostream>
 #include <chrono>
 #include <thread>
@@ -20,6 +25,8 @@ int main()
 	auto targetTimepoint = std::chrono::steady_clock::now();
 	float dt = (float)timeBetweenFrames.count() / std::chrono::microseconds(std::chrono::seconds(1)).count();
 
+	bool isRunning = false;
+
 	Particle p;
 
 	while (!window.isClosed()) {
@@ -30,11 +37,42 @@ int main()
 		Input::pollEvents();
 
 		Renderer::getInstance()->getCamera().update(dt);
-		p.applyForce({ 0.0f, -9.81f, 0.0f });
-		p.update(dt);
+
+		if (isRunning) {
+			p.applyForce({ 0.0f, -9.81f, 0.0f });
+			p.update(dt);
+		}
 
 		Renderer::getInstance()->clearScreen();
 		Renderer::getInstance()->renderParticle(p);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		ImGui::Begin("Simulation Manager");
+		if (isRunning) {
+			if (ImGui::Button("Pause Simulation")) {
+				isRunning = false;
+			}
+		}
+		else {
+			if (ImGui::Button("Play Simulation")) {
+				isRunning = true;
+			}
+		}
+
+		ImGui::InputFloat3("Position", &p.position.x);
+		ImGui::InputFloat3("Velocity", &p.velocity.x);
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 	return 0;
 }
